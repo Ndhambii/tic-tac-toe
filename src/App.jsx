2ba66f0cad5f577045dd96f58e94a466ecc02e50
Ndhambi Osvaldo T. Zitha <osvaldondhambi@gmail.com>
2025-01-04 14:11:22 +0200

    Determine next player and winner

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,11 @@ function Board() {
   const [xIsNext, setXIsNext] = useState(true);
 
   function handleSquareClick(index) {
+
+    if (squares[index] || calculateWinner(squares)) {
+      return;
+    }
+
     const auxSquares = squares.slice();
 
     if (xIsNext) {
@@ -34,8 +39,19 @@ function Board() {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  }
+  else {
+    status = "Next Player: " + (xIsNext ? "x" : "O");
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
+
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleSquareClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleSquareClick(1)} />
@@ -55,4 +71,20 @@ function Board() {
       </div>
     </>
   )
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+    [0, 4, 8], [2, 4, 6]            // Diagonals
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
